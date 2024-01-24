e88d4a35ed952626dcdd1f3e1fd0ee7975f7852a
Trac Van <truongtracvan@gmail.com>
2024-01-24 08:38:32 +0700

    Module2 Week2 Refactoring Variable

---
+++ apj-2023-fizzbuzz/src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(FizzBuzz.fizzBuzz(30) );
+        System.out.println(FizzBuzz.fizzBuzz(15));
+        System.out.println(FizzBuzz.fizzBuzz(5));
+        System.out.println(FizzBuzz.fizzBuzz(3));
+        System.out.println(FizzBuzz.fizzBuzz(8));
+
+
+    }
+}
